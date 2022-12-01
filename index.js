11ecd96d799eb7f08f808fed7a1608bc260af27a
Tanvir Kabir <tanvir@Tanvirs-MacBook-Pro.local>
2022-12-02 00:09:24 +0600

    full setup

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 const express = require('express')
+const cors = require('cors')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+
+const port = process.env.PORT || 4000;
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Phone gallery server is running')
+})
+
+app.listen(port, () => {
+    console.log('App running on port', port)
+})
