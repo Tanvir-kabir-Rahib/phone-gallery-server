03c2712ba90dfde55448fe8c81c4afcd40e7cdef
Tanvir Kabir <tanvir@Tanvirs-MacBook-Pro.local>
2022-12-02 02:11:11 +0600

    phone categroy send

--- index.js
+++ index.js
@@ -13,6 +13,22 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.thwifey.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        const categoryCollection = client.db("phoneGallery").collection("phoneCategory");
+        app.get('/category', async (req, res) => {
+            const query = {};
+            const result = await categoryCollection.find(query).toArray();
+            res.send(result)
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err));
+
 app.get('/', (req, res) => {
     res.send('Phone gallery server is running')
 })
